c45c41b9a773a2769f825c1f40bc6c6bab6449f9
Michael Simpson <michael@snthesis.com>
2020-01-17 20:25:54 -0500

    Update dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,43 @@
-extern crate colored;
-extern crate reqwest;
-extern crate scraper;
-
+use async_std::task;
 use colored::*;
 use reqwest::Error;
 use scraper::{ElementRef, Html, Selector};
 use std::result::Result;
 
-fn main() {
-  match get_body() {
-    Ok(body) => {
-      for element in Html::parse_document(&body).select(&selector()) {
-        parse_story(element);
-        parse_link(element);
-      }
+#[tokio::main]
+async fn main() {
+    match task::block_on(get_body()) {
+        Ok(body) => {
+            for element in Html::parse_document(&body).select(&selector()) {
+                parse_story(element);
+                parse_link(element);
+            }
+        }
+        Err(_) => eprintln!("Error fetching Hacker News data."),
     }
-    Err(_) => eprintln!("Error fetching Hacker News data."),
-  }
 }
 
-fn get_body() -> Result<String, Error> {
-  reqwest::get("https://news.ycombinator.com/")?.text()
+async fn get_body() -> Result<String, Error> {
+    reqwest::get("https://news.ycombinator.com/")
+        .await?
+        .text()
+        .await
 }
 
 fn selector() -> Selector {
-  Selector::parse("a.storylink").unwrap()
+    Selector::parse("a.storylink").unwrap()
 }
 
 fn parse_link(element: ElementRef) {
-  if let Some(link) = element.value().attr("href") {
-    let mut link = link.to_owned();
-    if link.starts_with("item?") {
-      link = format!("https://news.ycombinator.com/{}", link);
+    if let Some(link) = element.value().attr("href") {
+        let mut link = link.to_owned();
+        if link.starts_with("item?") {
+            link = format!("https://news.ycombinator.com/{}", link);
+        }
+        println!("\t{}", link);
     }
-    println!("\t{}", link);
-  }
 }
 
 fn parse_story(element: ElementRef) {
-  println!("{}", element.inner_html().cyan());
+    println!("{}", element.inner_html().cyan());
 }
